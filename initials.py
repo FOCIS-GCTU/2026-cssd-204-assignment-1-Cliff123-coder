d570fd0a76e8f6f2b453474e2802d185e702829e
Cliff123-coder <2425403620@live.gctu.edu.gh>
2026-05-31 16:29:43 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+#File: initials.py
+# Description: printing my initials in huge letters filled with mini letters
+# Assignment Number:2
+#
+# Name: Akweter Clifford Kpabiter
+# STUDENT ID:  2425403620
+# Email:cliffordakweter@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Akweter Clifford Kpabiter, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # printing my initials in huge letters filled with mini letters
 
+    print()
+    print("...ACK")
+    print()
+
+    print(".......AAAA.............CCCCCCCCCC.........KK......KKKK.....")
+    print("......AAAAAA...........CCC......CC.........KK....KKKK.......")
+    print(".....AAAAAAAA..........CC..................KK..KKKK.........")
+    print("....AAA....AAA.........CC..................KKKKKK...........")
+    print("...AAA......AAA........CC..................KKKKKK...........")
+    print("...AAAAAAAAAAAA........CC..................KK..KKKK.........")
+    print("...AAA......AAA........CC..................KK....KKKK.......")
+    print("...AAA......AAA........CC..................KK......KKKK.....")
+    print("...AAA......AAA...**...CCC......CC....**...KK.......KKK...**")
+    print("...AAA......AAA...**....CCCCCCCCCC....**...KK........KK...**")
+
+    print()
 
 
 main()
